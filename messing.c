cca9c355af73d440bfa3b2b4180e330c1d2e12f7
castellinho <guigacastellani@gmail.com>
2018-04-08 10:09:02 -0300

    if/else statements introduction

--- messing.c
+++ messing.c
@@ -3,15 +3,14 @@
 //variables section
 
 int num_1;       //declaration of integer var 'x'
-int num_2 = 5;   //declaration and initialization of integer var 'y'
 
 int main() {
 
-  printf("Hello!\n");                                           //prints welcoming to the screen
-  printf("Type an integer number: ");                            //prints instruction to the screen
-  scanf("%i", &num_1);                                            //scans input as digit; stores in variable
-  printf("%i little bunnies jumping on the grove...", num_1);      //outputs string with input
-  getchar();                                                        //waits non specified character input
+  printf("Hello!\n");   //prints welcoming to the screen
+  printf("Type an integer number: ");   //prints instruction to the screen
+  scanf("%i", &num_1);   //scans input as digit; stores in variable
+  printf("%i little bunnies jumping on the grove...", num_1);   //outputs string with input
+  getchar();   //waits non specified character input
 
   return 0;   //program successfully ('0') finished
 }
