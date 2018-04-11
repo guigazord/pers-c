c0c37295f07e0aed31007a1a26b3a3d76879c699
guigazord <guigacastellani@gmail.com>
2018-04-11 17:29:04 -0300

    for loop added, bunnies and deers counting :)

--- messing.c
+++ messing.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 //variables section
-int num_bunny, num_deer;       //declaration of integer var 'x'
+int num_bunny, num_deer, num_total;       //declaration of integer var 'x'
 
 int main() {
+
+  //basic input and outputs and variable functioning
 
   printf("Hello hunter!\n");   //prints welcoming to the screen
   printf("How many bunnies do you see? ");   //prints instruction to the screen
   scanf("%i", &num_bunny);   //scans input as digit; stores in variable
 
+  //if else statements
   if (num_bunny == 0) {
     printf ("Somebody's not going to eat today...");   //outputs string with input
   } else if (num_bunny > 0 && num_bunny <= 10) {
@@ -28,6 +31,24 @@ int main() {
   } else {
     printf ("Holy Cow! This wilderness is huge!");
   }
+
+  getchar();
+  printf("\nTotal of bunnies:\n");
+
+  //for loops
+  for (int wai = 1; wai <= num_bunny; wai++) {
+    printf ("%i\n", wai);
+  }
+
+  getchar();
+  printf("Total of deers:\n");
+
+  for (int jay = 1; jay <= num_deer; jay++) {
+    printf("%i\n", jay);
+  }
+
+  num_total = (num_bunny + num_deer);
+  printf("\nThat escalated quickly! %i animals in the jungle!", num_total);
 
   return 0;   //program successfully ('0') finished
 }
