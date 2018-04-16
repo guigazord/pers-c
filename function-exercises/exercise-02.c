e2f596634fbc11dbac659e504a62f1188d3093bc
guigazord <guigacastellani@gmail.com>
2018-04-16 20:19:04 -0300

    function exercise number 2 finalized: squared number

--- function-exercises/exercise-02.c
+++ function-exercises/exercise-02.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int x;
+
+int square_num(int num_sqr);
+
 int main() {
 
+  printf("Enter a number to be squared: ");
+  scanf("%i", &x);
+  printf("%i squared is equals to: %i", x, square_num(x));
+
   return 0;
+}
+
+int square_num(int num_sqr) {
+
+  return (num_sqr * num_sqr);
 }
